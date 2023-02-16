e72341e179e3ddf203b812bffdcbaf3324df7db5
cam.frt07@gmail.com <99878415+6oldenBerry@users.noreply.github.com>
2023-02-16 17:07:41 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-
 
 function App({ Component, pageProps }) {
   return (
